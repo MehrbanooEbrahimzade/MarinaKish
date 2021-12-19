e2d567dd9854a291fe6de79325986fa93edc33ea
mehrbanoo <mehrbanooebrahimzade@gmail.com>
2021-12-19 18:38:00 +0330

    model Percent & schedule

--- Infrastructure/Repository/interfaces/IFunRepository.cs
+++ Infrastructure/Repository/interfaces/IFunRepository.cs
@@ -8,74 +8,74 @@ namespace Infrastructure.Repository.interfaces
 {
     public interface IFunRepository
     {
-        /// <summary>
-        /// بررسی وجود داشتن تفریح
-        /// </summary>
-        Task<bool> CheckFunTypeIsExist(string name);
+        ///// <summary>
+        ///// بررسی وجود داشتن تفریح
+        ///// </summary>
+        //Task<bool> CheckFunTypeIsExist(string name);
 
-        /// <summary>
-        /// اضافه کردن تفریح به تِیبل :
-        /// </summary>
-        Task<bool> AddFunAsync(Fun fun);
+        ///// <summary>
+        ///// اضافه کردن تفریح به تِیبل :
+        ///// </summary>
+        //Task<bool> AddFunAsync(Fun fun);
 
-        /// <summary>
-        /// گرفتن تفریح با آیدی :
-        /// </summary>
-        Task<Fun> GetFunById(Guid id);
+        ///// <summary>
+        ///// گرفتن تفریح با آیدی :
+        ///// </summary>
+        //Task<Fun> GetFunById(Guid id);
 
-        /// <summary>
-        /// گرفتن تفریح فعال با آیدی :
-        /// </summary>
-        Task<Fun> GetActiveFunById(Guid id);
+        ///// <summary>
+        ///// گرفتن تفریح فعال با آیدی :
+        ///// </summary>
+        //Task<Fun> GetActiveFunById(Guid id);
 
-        /// <summary>
-        /// گرفتن تفریح غیرفعال با آیدی :
-        /// </summary>
-        Task<Fun> GetDisActiveFunById(Guid id);
+        ///// <summary>
+        ///// گرفتن تفریح غیرفعال با آیدی :
+        ///// </summary>
+        //Task<Fun> GetDisActiveFunById(Guid id);
 
-        /// <summary>
-        /// ذخیره کردن اعمال انجام شده
-        /// </summary>
-        Task<bool> UpdateFunAsync();
+        ///// <summary>
+        ///// ذخیره کردن اعمال انجام شده
+        ///// </summary>
+        //Task<bool> UpdateFunAsync();
 
-        /// <summary>
-        /// حذف تفریح
-        /// </summary>
-        Task<bool> DeleteFunAsync(Guid id);
+        ///// <summary>
+        ///// حذف تفریح
+        ///// </summary>
+        //Task<bool> DeleteFunAsync(Guid id);
 
-        /// <summary>
-        /// گرفتن همه تفریح ها
-        /// </summary>
-        Task<List<Fun>> GetAllFunAsync();
+        ///// <summary>
+        ///// گرفتن همه تفریح ها
+        ///// </summary>
+        //Task<List<Fun>> GetAllFunAsync();
 
-        /// <summary>
-        /// گرفتن تفریح با نوع تفریح
-        /// </summary>
-        Task<Fun> GetFunByFunType(string name);
+        ///// <summary>
+        ///// گرفتن تفریح با نوع تفریح
+        ///// </summary>
+        //Task<Fun> GetFunByFunType(string name);
 
-        /// <summary>
-        /// دریافت فایل با آیدی
-        /// </summary>
-        Task<File> GetFileById(Guid fileid);
+        ///// <summary>
+        ///// دریافت فایل با آیدی
+        ///// </summary>
+        //Task<File> GetFileById(Guid fileid);
 
-        /// <summary>
-        /// دریافت همه تفریح های فعال
-        /// </summary>
-        Task<List<Fun>> GetAllActivedFun();
+        ///// <summary>
+        ///// دریافت همه تفریح های فعال
+        ///// </summary>
+        //Task<List<Fun>> GetAllActivedFun();
 
-        /// <summary>
-        /// دریافت همه تفریح های غیر فعال
-        /// </summary>
-        Task<List<Fun>> GetAllDisActivedFun();
+        ///// <summary>
+        ///// دریافت همه تفریح های غیر فعال
+        ///// </summary>
+        //Task<List<Fun>> GetAllDisActivedFun();
 
-        /// <summary>
-        /// دریافت همه سانس های برگذار نشده فعال یک تفریح با آیدی
-        /// </summary>
-        Task<List<Schedule>> GetAllFunActiveSchedulesById(Guid funid);
+        ///// <summary>
+        ///// دریافت همه سانس های برگذار نشده فعال یک تفریح با آیدی
+        ///// </summary>
+        //Task<List<Schedule>> GetAllFunActiveSchedulesById(Guid funid);
 
-        /// <summary>
-        /// دریافت همه سانس های برگذار نشده غیرفعال یک تفریح با آیدی
-        /// </summary>
-        Task<List<Schedule>> GetAllFunDisActiveSchedulesById(Guid funid);
+        ///// <summary>
+        ///// دریافت همه سانس های برگذار نشده غیرفعال یک تفریح با آیدی
+        ///// </summary>
+        //Task<List<Schedule>> GetAllFunDisActiveSchedulesById(Guid funid);
     }
 }
