75b14ead913af073b5fd24266bfbeed07bfc4ede
mehrbanoo <mehrbanooebrahimzade@gmail.com>
2021-12-24 18:11:32 +0330

    AddApi(AddScheduleInfo)

---
+++ Application/Commands/Comment/ScheduleInfoCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Commands.Comment
+{
+    public class ScheduleInfoCommand
+    {
+        public TimeSpan StartTime { get; private set; }
+
+        public TimeSpan EndTime { get; private set; }
+
+        public int GapTime { get; private set; }
+        
+        public int Duration { get; private set; }
+
+        public int TotalCapacity { get; private set; }
+
+        
+        public int PresenceCapacity { get; private set; }
+
+        
+        public int OnlineCapacity { get; private set; }
+
+        
+        public decimal Amount { get; private set; }
+
+    }
+}
---
+++ Marina Club/Controllers/ScheduleInformationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Domain.Models;
+using Application.Commands.Comment;
+
+namespace Marina_Club.Controllers
+{
+    public class ScheduleInformationController : ControllerBase
+    {
+        public void AddScheduleInfo(ScheduleInfoCommand command)
+        {
+            var scheduleInfo = new ScheduleInfo(command.StartTime, command.EndTime, command.GapTime, command.Duration,
+                command.TotalCapacity, command.PresenceCapacity, command.OnlineCapacity, command.Amount);
+
+
+            //var schedule = new Schedule()
+            //    (DateTime date, TimeSpan start, TimeSpan end, decimal price, Guid funId
+        }
+
+    }
+}
