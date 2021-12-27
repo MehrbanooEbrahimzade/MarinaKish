b9963117b190d80ef2ee5fde0e11619fbc553c1a
mehrbanoo <mehrbanooebrahimzade@gmail.com>
2021-12-27 18:25:42 +0330

    add repository for ScheduleInfo

--- Application/Services/classes/ScheduleInfoService.cs
+++ Application/Services/classes/ScheduleInfoService.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Application.Commands.ScheduleInfo;
+using Application.Services.interfaces;
+using Infrastructure.Repository.interfaces;
 
 namespace Application.Services.classes
 {
     public class ScheduleInfoService
     {
-        public void AddScheduleInfoAsync()
+        private readonly IScheduleInfoRepository _scheduleInfoRepository;
+
+        public ScheduleInfoService(IScheduleInfoRepository scheduleInfoRepository)
+        {
+            _scheduleInfoRepository = scheduleInfoRepository;
+
+        }
+        public void AddScheduleInfoAsync(AddScheduleInfoCommand command)
         {
 
         }
---
+++ Infrastructure/Repository/interfaces/IScheduleInfoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository.interfaces
+{
+    public class IScheduleInfoRepository
+    {
+
+    }
+}
