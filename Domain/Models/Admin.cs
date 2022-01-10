3c17d69fcb9c5748fc9b63a0231e7f77758b509a
Mahyar Mohammdi <lilmahyar@protonmail.com>
2022-01-10 15:34:58 +0330

    Added context seed with roles

---
+++ Domain/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Admin : User
+    {
+        public Admin(string email, string password) : base(email)
+        {
+            Email = email;
+            PasswordHash = password; 
+        }
+        
+    }
+}
